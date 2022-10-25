9f0721bb03f4ca78975f582e17ef60183ba8f183
Srikanth08 <88487658+Srikanth0k@users.noreply.github.com>
2022-10-25 04:39:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//npm install express
+
+var express = require('express');
+
+var app = express();
+
+// set port, listen for requests
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+
+console.log(`Server is running on port ${PORT}.`);
+
+});
+
+// Default Route – http://localhost:8080
+
+app.get('/', function (req, res) {
+
+res.send('<h1>Hello World  Created By 2011CS010134  Srikanth</h1>');
+
+})
